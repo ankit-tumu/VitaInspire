f30bf9f6cf9dda4dc99123029dcd364a6bc80c87
ankit <ankit.tumuluri@gmail.com>
2025-07-30 15:21:11 -0400

    Add PostgreSQL support for persistent database storage on Render

--- config.py
+++ config.py
@@ -7,9 +7,8 @@ class Config:
     SECRET_KEY = os.getenv('FLASK_SECRET_KEY', 'a_super_secret_key_change_this')
     # Use Render's PostgreSQL or stick with SQLite for now
     if os.getenv('RENDER'):
-        # For Render PostgreSQL (if you add it later)
-        # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'vita_inspire.db')
+        # For Render PostgreSQL
+        SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'vita_inspire.db'))
     else:
         SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'vita_inspire.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
